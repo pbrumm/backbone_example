848755dd403ff37d654a07d15b5b6df774921314
root <root@grandview.(none)>
2012-09-13 20:40:22 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneExample::Application.config.secret_token = '00e4086bb1cd059e9a8a6e4aa789e8dadf5111e70025eabf613280379151e6d491b9b91f7832f72eec35094fc47c0166922d6d4600d33a919368e0f86eb4a0e0'
